ed24959abf4506a30f3427f4455d0780ee4f061e
NatalieDobrovolska <148444779+NatalieDobrovolska@users.noreply.github.com>
2024-09-10 21:19:38 +0200

    Add files via upload

---
+++ my_module.py
@@ -0,0 +1,9 @@
+# my_module.py
+
+def multiply(a, b):
+    """Returnerar produkten av två tal."""
+    return a * b
+
+def greet(name):
+    """Returnerar en hälsning."""
+    return f"Hej, {name}!"
